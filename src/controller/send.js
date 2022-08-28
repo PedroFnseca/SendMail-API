1edccc3838284abbd0c6370fb028477d4f026b3e
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-27 22:10:37 -0300

    Controller

---
+++ src/controller/send.js
@@ -0,0 +1,63 @@
+import express from 'express'
+import { body, validationResult } from 'express-validator'
+import { sendMailHTML } from '../service/sendMail'
+
+const router = express.Router()
+
+// Endpoint: /html
+// Tipo: POST
+// Descrição: Envia email com html
+router.post('/html', [
+    body('to').isEmail().withMessage('E-mail inválido'),
+    body('subject').isString().withMessage('Assunto inválido'),
+    body('html').isString().withMessage('Html inválido')
+], async (req, res) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).json({ errors: errors.array() })
+    }
+
+    const { to, subject, html } = req.body
+
+    try {
+        await sendMailHTML(to, subject, html)
+        res.status(200).json({
+            message: 'Email enviado com sucesso'
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            error: err.message
+        })
+    }
+})
+
+// Endpoint: /text
+// Tipo: POST
+// Descrição: Envia email com texto simples
+router.post('/text', [
+    body('to').isEmail().withMessage('E-mail inválido'),
+    body('subject').isString().withMessage('Assunto inválido'),
+    body('text').isString().withMessage('Texto inválido')
+], async (req, res) => {
+    
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).json({ errors: errors.array() })
+    }
+
+    const { to, subject, text } = req.body
+
+    try {
+        await sendMailText(to, subject, text)
+        res.status(200).json({
+            message: 'Email enviado com sucesso'
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            error: err.message
+        })
+    }
+})
